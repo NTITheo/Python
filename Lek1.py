00150d0f2a11a946bf1e54638f7317a7e9ff5874
Theo Larefalk <theo.larefalk@learnet.se>
2019-09-03 10:09:01 +0200

    första 80 min

--- Lek1.py
+++ Lek1.py
@@ -1,1 +1,9 @@
 print("Hej fungerar det här?")
+
+firstName = "Theo"
+lastName = 'Larefalk'
+teacher = False
+dateOfBirth = 20010122
+age = 18
+print(firstName, lastName, age, teacher)
+print(dateOfBirth)

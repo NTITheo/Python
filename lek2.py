05ebba092853880aaa54bb341702ac3c2675b5af
Theo Larefalk <theo.larefalk@learnet.se>
2019-09-03 10:55:01 +0200

    blablueblae

---
+++ lek2.py
@@ -0,0 +1,22 @@
+# Variabler för att komma ihåg namn och ålder
+#restriction = "welcome"
+#minimum = 18
+#try:
+
+
+#if age < 18:
+    print("blank is not possible")    
+else:
+    print("blank is possible")
+
+
+# läsa in namn
+Name = input("Skriv in ditt namn: ")
+
+# läsa in ålder
+age = int(input("Hur gammal är du: "))
+
+# skriva ut hej namn och du är x gammal
+print("Hejsan", Name,", välkommen till mitt program.")
+print("Du är ", age , " gammal!")
+
---
+++ wiki.md
@@ -0,0 +1,9 @@
+ # Pythonintro
+
+ # variabler
+
+
+ 
+
+
+
